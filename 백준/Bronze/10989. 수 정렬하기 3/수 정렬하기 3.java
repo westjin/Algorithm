a703653e61c7899bf7c5986fc4e08b14367dfdd8
seojinyang <95288764+westjin@users.noreply.github.com>
2025-01-06 17:04:06 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2420 ms, Memory: 340636 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+        Radix_Sort(A,5);
+        for (int i = 0; i < N; i++) {
+            bw.write(A[i]+"\n");
+        }
+        bw.flush();
+        bw.close();
+        
+    }
+
+    public static void Radix_Sort(int[] A, int max_size){
+        int[] output = new int[A.length];
+        int jarisu = 1;
+        int count = 0;
+        while (count != max_size){
+            int[] bucket = new int[10];
+            for (int i = 0; i < A.length; i++) {
+                bucket[(A[i] / jarisu) % 10]++; //해당 값의 특정 자릿수를 얻기 위해 사용합니다. ex) 123 / 1 = 123, 123 % 10 = 3
+            }
+
+            for (int i = 1; i < 10; i++) {
+                bucket[i] += bucket[i-1];
+            }
+
+            for (int i = A.length - 1; i >= 0 ; i--) { //현재 자릿수를 기준으로 정렬하기
+                output[bucket[(A[i] / jarisu % 10)] -1 ] = A[i];
+                bucket[(A[i] / jarisu) % 10]--;
+            }
+            for (int i = 0; i < A.length; i++) {
+                //다음 자릿수를 이덩하기 위해 현재 자릿수 기준 정렬 데이터 저장하기
+                A[i] = output[i];
+            }
+            jarisu = jarisu * 10; // 자릿수 증가 시키기
+            count++;
+        }
+    }
+
+}
