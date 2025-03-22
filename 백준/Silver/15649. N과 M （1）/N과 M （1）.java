407b2707f5081633ec216de26e17c7cda90583dd
seojinyang <95288764+westjin@users.noreply.github.com>
2025-03-22 18:23:53 +0900

    [Silver III] Title: N과 M (1), Time: 1448 ms, Memory: 48560 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,41 +6,38 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static boolean v[];
-    static int arr[];
+    static boolean[] v;
+    static int[] result;
+    static int N,M;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
         v = new boolean[N+1];
-        arr = new int[M];
-        DFS(0,M,N);
+        result = new int[M];
 
-
-
+        DFS(0);
 
     }
-    public static void DFS(int depth, int M, int N){
+    public static void DFS(int depth){
         if(depth == M){
-            //출력
             for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
+                System.out.print(result[i] + " ");
             }
-            System.out.println();
             return;
         }
+
         for (int i = 1; i <= N; i++) {
             if(!v[i]){
                 v[i] = true;
-                arr[depth] = i;
-                DFS(depth+1, M, N);
+                result[depth] = i;
+                DFS(depth+1);
                 v[i] = false;
             }
         }
-
     }
 
 }
