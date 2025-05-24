d8f97debc85551711f1e81f78fde6645c4aaeb9c
seojinyang <95288764+westjin@users.noreply.github.com>
2025-05-24 20:52:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.61 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,47 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
-        // BFS로 최단거리 구하기
+        //BFS로 최단 거리 구하기
         
-        // 시작 점 예약 (0,0, 거리 1)
-        int[] dr = {1,-1,0,0};
-        int[] dc = {0,0,1,-1};
+        //상하좌우 움직임 
+        int[] dr = {1,-1,0,0}; 
+        int[] dc = {0,0,-1,1};
+        
         int n = maps.length;
         int m = maps[0].length;
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
         
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] v = new boolean[n][m];
+        q.offer(new int[]{0,0,1});
         
-        queue.offer(new int[]{0,0,1});
-        visited[0][0] = true;
-        
-        while(!queue.isEmpty()){
-        //현재 노드 방문
-            int[] cur = queue.poll();
+        while(!q.isEmpty()){
+            //현재 노드 꺼내기
+            int[] cur = q.poll();
             int r = cur[0], c = cur[1], dist = cur[2];
             
-        
-        //if 도착지 도달하면 Return dist;
-        if(r == n-1 && c == m -1) return dist;
-        //다음 노드 예약
+            //목적지 도달 시 거리 반환
+            if(r == n -1 && c == m - 1) return dist;
+            //다음 노드 찾기
             for(int i = 0; i < 4; i++){
-                int nr = r +dr[i], nc = c + dc[i];
-                if(nr >= 0&& nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1){
-                    if(!visited[nr][nc]){
-                        queue.offer(new int[]{nr,nc,dist + 1});
-                        visited[nr][nc] =true;
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                //다음 노드 유효성 검사
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1){
+                    if(!v[nr][nc]){
+                        q.offer(new int[]{nr,nc,dist + 1});
+                        v[nr][nc] = true;
                     }
                 }
-            
+                
             }
-        
+            
+            
+            
+            
         }
         
-        
-        
+
         return -1;
     }
 }
