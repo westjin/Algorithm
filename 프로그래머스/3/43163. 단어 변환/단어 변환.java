875acb2103fb942e689ea79abe07c571976f2ce1
seojinyang <95288764+westjin@users.noreply.github.com>
2025-05-25 00:33:41 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,49 @@
 import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int level =  bfs(begin,target,words);
+
+        int level = bfs(begin,target,words);
         return level;
     }
     
     public int bfs(String begin, String target, String[] words){
-        Queue<String> q = new LinkedList<>();
-        boolean[] v = new boolean[words.length]; 
+        Queue<String> q = new ArrayDeque<>();
+        boolean[] v = new boolean[words.length];
         int level = 0;
         q.add(begin);
         
         while(!q.isEmpty()){
-            int size = q.size(); // 이번 레벨에 몇 개 노드 있는지
-            for(int s = 0; s < size; s++) {
-                String curVertex = q.remove();
+            int size = q.size(); //이번 레벨에 존재하는 노드의 개수
+            for(int i = 0; i < size; i++){
+                String curVertex = q.poll(); //현재 노드 꺼내기
                 if(curVertex.equals(target)){
                     return level;
                 }
-                for(int i = 0; i < words.length; i++){
-                    if(!v[i] && getDiffCount(curVertex, words[i]) == 1){
-                        q.add(words[i]);
-                        v[i] = true;
-                    }
+            
+            for(int j = 0; j < words.length; j++){
+                if(!v[j] && getDiffCount(curVertex,words[j]) == 1){
+                    v[j] = true;
+                    q.add(words[j]);
                 }
             }
-            level++; // 이 레벨 size개 다 꺼내고 나서 level 1 올린다
         }
-        
-        return 0;
+            level ++;
     }
+    return 0;
+}
+
     
-    public int getDiffCount(String word, String target){
+    public int getDiffCount(String word, String target ){
         int diffCount = 0;
-        for(int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++){
             if(word.charAt(i) != target.charAt(i)){
-                diffCount++;
+                diffCount ++;
             }
         }
+        
         return diffCount;
     }
+    
+    
     
 }
