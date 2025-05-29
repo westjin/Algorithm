fb6aac8a1b37d6c1c2aeb473f60b57ec6982d58f
seojinyang <95288764+westjin@users.noreply.github.com>
2025-05-30 02:27:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.51 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,44 +4,45 @@ class Solution {
     public int solution(int n, int[][] wires) {
         answer = n;
         
+        //그래프 만들기
         Map<Integer,List<Integer>> graph = new HashMap<>();
         
         //그래프 초기화
         for(int i = 1; i <= n; i++){
             graph.put(i,new ArrayList<>());
         }
-        
-        //그래프 값 채우기
+        //그래프 연결
         for(int[] wire : wires){
             graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);   
+            graph.get(wire[1]).add(wire[0]);
         }
+        boolean[] v = new boolean[n+1];
         
         //dfs 수행
-        boolean[] v = new boolean[n+1];
         dfs(graph,v,1,n);
-        
+       
         return answer;
     }
     
-    int dfs(Map<Integer, List<Integer>> graph, boolean[] v, int cur, int n){
+    public int dfs(Map<Integer, List<Integer>> graph, boolean[] v,int cur, int n){
+        //현재 노드 방문
         int count = 1;
         v[cur] = true;
         
+        //다음 노드 방문
         for(int next : graph.get(cur)){
             if(!v[next]){
-                count += dfs(graph, v, next, n);
-            }   
+                count += dfs(graph,v,next,n);
+            }
         }
-        //최적값 업데이트
-        answer = Math.min(answer, Math.abs(n-count*2));
         
-        //자기 자신ㅇ 아래의 노드 수 반환
+        //최적값 업데이트
+        answer = Math.min(answer,Math.abs(n - count * 2));
+        
         
         return count;
         
+        
     }
-    
-    
     
 }
