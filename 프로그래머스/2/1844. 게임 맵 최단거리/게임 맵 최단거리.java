fd8254574d42f2beee3c6d739119db2959756f20
seojinyang <95288764+westjin@users.noreply.github.com>
2025-05-29 18:51:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.66 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,42 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
-        //BFS로 최단 거리 구하기
-        
-        //상하좌우 움직임 
-        int[] dr = {1,-1,0,0}; 
+        //상하좌우
+        int[] dr = {1,-1,0,0};
         int[] dc = {0,0,-1,1};
         
         int n = maps.length;
-        int m = maps[0].length;
+        int m = maps[0].length; 
         
-        Queue<int[]> q = new ArrayDeque<>();
         boolean[][] v = new boolean[n][m];
-        q.offer(new int[]{0,0,1});
+        Queue<int[]> q = new ArrayDeque<>();
+        
+        //큐에 시작점 넣기
+        q.offer(new int[] {0,0,1});
+        v[0][0] = true;
         
         while(!q.isEmpty()){
-            //현재 노드 꺼내기
-            int[] cur = q.poll();
+            int cur[] = q.poll();
             int r = cur[0], c = cur[1], dist = cur[2];
             
-            //목적지 도달 시 거리 반환
-            if(r == n -1 && c == m - 1) return dist;
-            //다음 노드 찾기
+            //도착지 도착이라면 dist return
+            if(r == n-1 && c == m-1) return dist;
+            
+            //다음 노드 예약 ( 현재 위치로부터 이동)
             for(int i = 0; i < 4; i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                //다음 노드 유효성 검사
                 if(nr >= 0 && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1){
+                    //방문 여부 확인하기
                     if(!v[nr][nc]){
-                        q.offer(new int[]{nr,nc,dist + 1});
-                        v[nr][nc] = true;
+                    q.offer(new int[] {nr,nc,dist+1});
+                    v[nr][nc] = true;
                     }
                 }
-                
             }
-            
-            
-            
             
         }
         
-
         return -1;
     }
 }
