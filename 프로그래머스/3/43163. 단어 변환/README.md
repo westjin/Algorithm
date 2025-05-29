52e47c1045c411795ead4d82aeb591c68a57848d
seojinyang <95288764+westjin@users.noreply.github.com>
2025-05-30 00:44:26 +0900

    [level 3] Title: 단어 변환, Time: 0.36 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 0.35 ms
+메모리: 86.3 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 25일 00:33:38
+2025년 05월 30일 00:44:24
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,53 @@
 import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-
         int level = bfs(begin,target,words);
         return level;
     }
     
+    
     public int bfs(String begin, String target, String[] words){
         Queue<String> q = new ArrayDeque<>();
         boolean[] v = new boolean[words.length];
+        //level 별 분린하기
         int level = 0;
         q.add(begin);
         
         while(!q.isEmpty()){
-            int size = q.size(); //이번 레벨에 존재하는 노드의 개수
-            for(int i = 0; i < size; i++){
-                String curVertex = q.poll(); //현재 노드 꺼내기
+            //이번 레벨에 큐가 몇 개 있는지 확인
+            int size = q.size();
+            for(int s = 0; s < size; s++){
+                //현재 노드 꺼내기
+                String curVertex = q.poll();
+                //이번 레벨에 정답이 있는지
                 if(curVertex.equals(target)){
                     return level;
                 }
-            
-            for(int j = 0; j < words.length; j++){
-                if(!v[j] && getDiffCount(curVertex,words[j]) == 1){
-                    v[j] = true;
-                    q.add(words[j]);
+                //다음 노드 예약하기
+                for(int i =0; i < words.length; i++){
+                    //방문하지 않았고, 차이가 1이라면
+                    if(!v[i] && getDiffCount(curVertex,words[i]) == 1){
+                        q.add(words[i]);
+                        v[i] = true;
+                    }
                 }
             }
+            //이번 레벨 체크 완료 다음 레벨로
+            level ++;     
         }
-            level ++;
+     return 0;   
     }
-    return 0;
-}
-
     
-    public int getDiffCount(String word, String target ){
+    public int getDiffCount(String word, String target){
         int diffCount = 0;
-        for (int i = 0; i < word.length(); i++){
+        for(int i = 0; i < word.length(); i++){
             if(word.charAt(i) != target.charAt(i)){
                 diffCount ++;
             }
         }
         
         return diffCount;
+        
     }
-    
-    
-    
 }
