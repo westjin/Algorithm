c905beb964cae197b250a38860e70a113d2d9030
seojinyang <95288764+westjin@users.noreply.github.com>
2025-11-19 17:12:44 +0900

    [level 2] Title: 소수 찾기, Time: 34.93 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 98.7 MB, 시간: 31.33 ms
+메모리: 98.1 MB, 시간: 34.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 10일 23:28:09
+2025년 11월 19일 17:12:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,6 +1,56 @@
+import java.util.*;
+
 class Solution {
+    static Set<Integer> set = new HashSet<>();
+    
     public int solution(String numbers) {
         int answer = 0;
+        char[] digits = numbers.toCharArray();
+        boolean[] visited = new boolean[numbers.length()];
+        backtrack("",digits,visited);
+        for(int num : set){
+            if(isPrime(num)){
+                answer ++;
+            }
+        }
+        
         return answer;
     }
+    public void backtrack(String curr, char[] digits,boolean[] visited){
+        
+        // 1. 지금까지 만든 문자열이 비어있지 않다면 → 숫자로 바꿔보기
+        if(!curr.equals("")){
+            int num = Integer.parseInt(curr);
+            // 여기서 소수 판별해서 set에 넣든지,
+            // 아니면 일단 set에 넣고 나중에 한꺼번에 isPrime 돌리든지 (둘 다 가능)
+            set.add(num);
+        }
+        
+        //2. 
+        for(int i = 0; i < digits.length; i ++){
+            if(!visited[i]){
+                visited[i] = true;
+                backtrack(curr + digits[i], digits, visited);
+                visited[i] = false;
+            }
+        }
+        
+        
+        
+    }
+    
+    public boolean isPrime(int n){
+        if(n < 2){
+            return false;
+        }
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    
 }
